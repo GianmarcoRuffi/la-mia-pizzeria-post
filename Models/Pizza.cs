e0da99e7b109ba146309feb8fe6b44229a6726e7
GianmarcoRuffi <gianmarco.ruffi@outlook.it>
2022-11-22 15:24:55 +0100

    Esercizio 1

--- Models/Pizza.cs
+++ Models/Pizza.cs
@@ -6,19 +6,20 @@ namespace la_mia_pizzeria_static.Models
     {
         public int Id { get; set; }
 
-        [Required(ErrorMessage = "Il campo è obbligatorio")]
+        [Required(ErrorMessage = "La pizza deve avere un nome")]
         [StringLength(40, ErrorMessage = "Il nome non può avere più di 40 caratteri")]
         public string? Name { get; set; }
 
-        [Required(ErrorMessage = "Il campo è obbligatorio")]
-        [StringLength(200, ErrorMessage = "La descrizione non può avere più di 200 caratteri")]
+        [Required(ErrorMessage = "La pizza deve avere una descrizione")]
+        [StringLength(300, ErrorMessage = "La descrizione non può avere più di 300 caratteri")]
         public string? Description { get; set; }
 
-        [Required(ErrorMessage = "Il campo è obbligatorio")]
+        [Required(ErrorMessage = "Devi inserire un link all'immagine della pizza")]
+        [StringLength(500, ErrorMessage = "Il link non può avere più di 500 caratteri")]
         public string? Image { get; set; }
 
-        [Required(ErrorMessage = "Il campo è obbligatorio")]
-        [Range(1, 999, ErrorMessage = "La pizza deve avere un prezzo valido")]
+        [Required(ErrorMessage = "La pizza deve avere un prezzo valido")]
+        [Range(1, 500, ErrorMessage = "La pizza deve avere un prezzo valido")]
         public double? Prezzo { get; set; }
 
 
