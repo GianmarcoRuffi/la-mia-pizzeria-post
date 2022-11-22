c894bf9cf9baec8442f0a55a4578df911e6905aa
GianmarcoRuffi <gianmarco.ruffi@outlook.it>
2022-11-22 13:46:10 +0100

    Aggiungere i file di progetto.

---
+++ Controllers/PizzaController.cs
@@ -0,0 +1,30 @@
+﻿using la_mia_pizzeria_static.Data;
+using la_mia_pizzeria_static.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace la_mia_pizzeria_static.Controllers
+{
+    public class PizzaController : Controller
+
+ 
+    {
+        public IActionResult Index()
+        {
+            PizzaDbContext db = new PizzaDbContext();
+
+            List<Pizza> listaPizza = db.Pizzas.ToList();
+
+        return View(listaPizza);
+        }
+
+        public IActionResult Dettaglio(int Id)
+        {
+
+            PizzaDbContext db = new PizzaDbContext();
+
+            Pizza pizza = db.Pizzas.Where(item => item.Id == Id).FirstOrDefault();
+
+            return View(pizza);
+        }
+    }
+}
---
+++ Models/Pizza.cs
@@ -0,0 +1,32 @@
+﻿namespace la_mia_pizzeria_static.Models
+{
+    public class Pizza
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+
+        public string Image { get; set; }
+
+        public double Prezzo { get; set; }
+
+
+        public Pizza() {
+        }
+
+        public Pizza(string name, string description, string image, double prezzo) {
+
+            this.Name = name;
+            this.Description = description;
+            this.Image= image;
+            this.Prezzo= prezzo;
+}
+    }
+
+
+
+
+
+
+
+}
